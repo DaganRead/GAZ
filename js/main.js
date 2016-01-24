3c955ed1f00bd1bc85d773090a112581bf509eba
Dagan Read <daganread@gmail.com>
2016-01-25 01:24:56 +0200

    debug

--- js/main.js
+++ js/main.js
@@ -17,39 +17,73 @@ var app = {
             locations:[]
         },
         simulate : function(evt) {
-            alert('click');
           evt.preventDefault();
           var el = document.body;
           var touches = evt.changedTouches;
-                
-          for (var i = 0; i < touches.length; i++) {
-            var event = new MouseEvent('click', {
-                'view': window,
-                'bubbles': true,
-                'cancelable': true
-            });
-            touches[i].target.dispatchEvent(event);
-          }
-        },
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
+            for (var i = 0; i < touches.length; i++) {
+                var tabs = touches[i].target.parentNode.parentNode.children;
+                switch(touches[i].target.nodeName){
+                    case "LABEL" :
+                    for (var ii = 0; ii < tabs.length; ii++) {
+                            tabs[ii].children[0].checked = false;
+                        };
+                        touches[i].target.control.checked = true;
+                        touches[i].target.control.onclick.call(touches[i].target.control);
+                        break;
+                    case "INPUT" :
+                        alert('trigger');
+                        break;
+                };      
+            };
     },
-
-    initialize: function() {
-        alert("Hello World!");
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
-
+    store : function(type) {
+            switch(type){
+                        case "sale" :
+                            var temp = JSON.parse(window.localStorage['data']);
+                            temp.sales = this.data.sales;
+                            window.localStorage['data'] = JSON.stringify(temp);
+                            alert(window.localStorage['data']);
+                            break;
+                        case "customer" :
+                            var temp = JSON.parse(window.localStorage['data']);
+                            temp.customers = this.data.customers;
+                            window.localStorage['data'] = JSON.stringify(temp);
+                            alert(window.localStorage['data']);
+                            break;
+                        case "item" :
+                            var temp = JSON.parse(window.localStorage['data']);
+                            temp.items = this.data.items;
+                            window.localStorage['data'] = JSON.stringify(temp);
+                            alert(window.localStorage['data']);
+                            break;
+                        case "slaughter" :
+                            var temp = JSON.parse(window.localStorage['data']);
+                            temp.slaughters = this.data.slaughters;
+                            window.localStorage['data'] = JSON.stringify(temp);
+                            alert(window.localStorage['data']);
+                            break;
+                        case "location" :
+                            var temp = JSON.parse(window.localStorage['data']);
+                            temp.locations = this.data.locations;
+                            window.localStorage['data'] = JSON.stringify(temp);
+                            alert(window.localStorage['data']);
+                            break;
+            }
+        },
+        initialize: function() {
+            alert("Hello World!");
+            this.store = new MemoryStore();
+            $('.search-key').on('keyup', $.proxy(this.findByName, this));
+            if (!window.localStorage['installed']) {
+                window.localStorage['installed'] = true;
+                window.localStorage['data'] = JSON.stringify({
+                                        sales:[],
+                                        customers:[],
+                                        items:[],
+                                        slaughters:[],
+                                        locations:[]
+                                    });
+                alert('installing...');
+            };        
+        }()
 };
-
-app.initialize();

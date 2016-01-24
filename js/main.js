d57288f0cd25a050564306934e8213d89ac63873
Dagan Read <daganread@gmail.com>
2016-01-24 17:51:54 +0200

    debug

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-alert('JS');
+document.addEventListener("deviceReady", deviceReady, false);
+
+function deviceReady() {
+    // Now safe to use the PhoneGap API
+    navigator.notification.alert('Hello', ok, 'Title', 'Button!');
+    alert('JS');
+}
 (function( window ) {
 	var app = {
 		DOM : {

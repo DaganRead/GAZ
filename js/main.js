458aa5c26dae57b5aa8603563cb34e4dd641ccde
Dagan Read <daganread@gmail.com>
2016-03-20 22:38:49 +0200

    update sale

--- js/main.js
+++ js/main.js
@@ -1493,6 +1493,7 @@ function onDeviceReady() {
                         };
                     });
                     app.store('sale');
+                    app.store('slaughter');
                 }();
 
             },

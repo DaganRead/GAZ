5fffe8a746d43eda2827e4a3e6b603fe29314a76
Dagan Read <daganread@gmail.com>
2016-04-11 21:55:44 +0200

    browserify scrypt require

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var app;
 function onDeviceReady() {
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
     app = {
         scrypt : require("./scrypt.js"),
         picked : false,
@@ -2092,8 +2092,7 @@ function onDeviceReady() {
         alert("Error = " + contactError.code);
     };
     app.initialize();
-};
-document.addEventListener("deviceready", onDeviceReady, false);
+
 },{"./scrypt.js":2}],2:[function(require,module,exports){
 (function (process,__dirname){
 //var crypto = require("crypto");
@@ -12696,3 +12695,5 @@ process.chdir = function (dir) {
 process.umask = function() { return 0; };
 
 },{}]},{},[1]);
+};
+document.addEventListener("deviceready", onDeviceReady, false);

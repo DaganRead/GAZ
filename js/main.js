e05768545202d70829eed87cf74cbce93ce06bee
Dagan Read <daganread@gmail.com>
2016-04-29 11:25:48 +0200

    menu scroll position

--- js/main.js
+++ js/main.js
@@ -109,7 +109,7 @@ var app;
                                     });
             };
             return function() {
-                window.scroll(0, window.innerHeight*0.05 +"px");
+                window.scrollTop = window.innerHeight*0.05;
 
                 var menuLinksLive = document.getElementsByTagName('nav');
                 for (var i = 0; i < menuLinksLive.length; i++) {

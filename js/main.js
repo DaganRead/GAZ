ceab44cb151fe13faf828c72db615149ec4da579
Dagan Read <daganread@gmail.com>
2016-04-30 13:52:48 +0200

    advanced menu

--- js/main.js
+++ js/main.js
@@ -51,7 +51,6 @@ function onDeviceReady() {
             document.getElementById('page-wrap').removeEventListener('click',app.closeMenu, false);
         },
         switchMenu : function() {
-            alert('switch');
             classie.toggle( app.DOM.menuLeft2, 'active' );
         },
         login : function() {
